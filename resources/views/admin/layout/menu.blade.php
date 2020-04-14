38b0f7f9d69e0fa6d33a99b903ca011d60e4833f
tuchithanh1998 <tuchithanh1998@gmail.com>
2020-04-14 19:29:27 +0700

    add update

--- resources/views/admin/layout/menu.blade.php
+++ resources/views/admin/layout/menu.blade.php
@@ -7,7 +7,7 @@
     
 
 
-<li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Nhóm tin</span> <span class="label label-important">2</span></a>
+<li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Nhóm tin</span> <span class="label label-important"></span></a>
       <ul>
         <li><a href="admin/nhomtin/danhsach.html">Danh sách</a></li>
         <li><a href="admin/nhomtin/them.html">Thêm</a></li>
@@ -16,14 +16,14 @@
     </li>
 
 
-    <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Loại tin</span> <span class="label label-important">2</span></a>
+    <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Loại tin</span> <span class="label label-important"></span></a>
       <ul>
         <li><a href="admin/loaitin/danhsach.html">Danh sách</a></li>
         <li><a href="admin/loaitin/them.html">Thêm</a></li>
 
       </ul>
     </li>
-    <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Tin</span> <span class="label label-important">2</span></a>
+    <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Tin</span> <span class="label label-important"></span></a>
       <ul>
         <li><a href="admin/tin/danhsach.html">Danh sách</a></li>
         <li><a href="admin/tin/them.html">Thêm</a></li>
@@ -31,7 +31,7 @@
       </ul>
     </li>
 
-    <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Bình luận</span> <span class="label label-important">2</span></a>
+    <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Bình luận</span> <span class="label label-important"></span></a>
       <ul>
          <li><a href="admin/binhluan/danhsach1.html">Danh sách chưa duyệt</a></li>
         <li><a href="admin/binhluan/danhsach.html">Danh sách đã duyệt</a></li>
